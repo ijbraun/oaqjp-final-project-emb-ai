b43016719161328419029d38bac4c76f7108b3bb
ijbraun <9153212+ijbraun@users.noreply.github.com>
2025-04-28 15:51:53 -0400

    formatted response

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,5 +6,23 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document": {'text':text_to_analyze}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url,json=myobj,headers=header)
+    formatted_response = json.loads(response.text)
+    
+    emotion = formatted_response['emotionPredictions']['emotion']
 
-    return response.text
+    anger_score = emotion['anger']
+    disgust_score = emotion['disgust']
+    fear_score = emotion['fear']
+    joy_score = emotion['joy']
+    sadness_score = emotion['sadness']
+    dominant_score = max(emotion.values,key=emotion.get)
+
+
+    return {
+        'anger':anger_score,
+        'disgust':disgust_score,
+        'fear': fear_score,
+        'joy':joy_score,
+        'sadness':sadness_score,
+        'dominant_emotion':dominant_score
+    }
