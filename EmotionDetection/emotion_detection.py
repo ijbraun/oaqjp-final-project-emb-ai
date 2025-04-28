a94786f54ab255aa6204fa6feeccc3571bbd78aa
ijbraun <9153212+ijbraun@users.noreply.github.com>
2025-04-28 16:27:26 -0400

    added testing

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,15 +7,15 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url,json=myobj,headers=header)
     formatted_response = json.loads(response.text)
-    
-    emotion = formatted_response['emotionPredictions']['emotion']
+
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
 
     anger_score = emotion['anger']
     disgust_score = emotion['disgust']
     fear_score = emotion['fear']
     joy_score = emotion['joy']
     sadness_score = emotion['sadness']
-    dominant_score = max(emotion.values,key=emotion.get)
+    dominant_key = max(emotion,key=emotion.get)
 
 
     return {
@@ -24,5 +24,5 @@ def emotion_detector(text_to_analyze):
         'fear': fear_score,
         'joy':joy_score,
         'sadness':sadness_score,
-        'dominant_emotion':dominant_score
+        'dominant_emotion':dominant_key
     }
